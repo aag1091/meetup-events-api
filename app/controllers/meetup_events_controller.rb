64919357991ba73502d6f007bbe94bb3ee7726e5
Avinash Gosavi <avinash.gosavi@forrent.com>
2015-11-10 20:12:50 -0500

    Added groups to response

--- app/controllers/meetup_events_controller.rb
+++ app/controllers/meetup_events_controller.rb
@@ -3,9 +3,14 @@ class MeetupEventsController < ApplicationController
   def show
     @interest_group = InterestGroup.find_by(tag: params[:tag])
     @meetup_events = MaEvent.new(@interest_group).data
+    @meetup_groups = MaGroup.new.data
     respond_to do |format|
       format.html { render :show }
-      format.json { render json: @meetup_events.to_json }
+      format.json { render json: {
+          groups: @meetup_groups,
+          events: @meetup_events 
+        }.to_json
+      }
     end
   end
 
